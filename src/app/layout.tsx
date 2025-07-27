ff9bc3b69b1476c12be900fa1aed1fac7768e008
Justin Rau <justin@foolsparadise.de>
2025-07-27 18:28:17 +0200

    feat(styling): use next/font font loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,24 @@
 import "@/styles/_index.scss";
 import React, {ReactNode, Suspense} from "react";
+import localFont from "next/font/local";
+import {Inter} from "next/font/google";
+import classNames from "~/classnames";
+
+const tttravels = localFont({
+  src: "../assets/TT Travels Next Bold.otf",
+  display: "swap",
+  variable: "--tttravels",
+});
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--inter",
+});
 
 export default function RootLayout({children}: {children: ReactNode}) {
   return (
-    <html lang="de">
-      <body className={"surface fp-base-style-scope"} style={{overflowX: "hidden"}}>
+    <html lang="de" className={classNames(inter.variable, tttravels.variable)}>
+      <body className={"fp-base-style-scope"} style={{overflowX: "hidden"}}>
         {children}
       </body>
     </html>
